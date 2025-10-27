293dadd9e57b44582bad689b9131974c06998222
Roshnab Afraz <roshnabafrazz@gmail.com>
2025-10-27 11:15:36 +0500

    added Random Facts feature.

--- app/src/main/java/com/roshnab/brainbites/screens/HomeScreen.kt
+++ app/src/main/java/com/roshnab/brainbites/screens/HomeScreen.kt
@@ -1,18 +1,15 @@
 package com.roshnab.brainbites.screens
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
